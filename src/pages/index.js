9c146c44bd985faa6221ec98275b3b727873c955
Rizchel Dayao <rizchel.a.dayao@gmail.com>
2019-04-08 18:45:42 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
+    <h1>Hi people! Testing, testing</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>It is time to build something amazing.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
