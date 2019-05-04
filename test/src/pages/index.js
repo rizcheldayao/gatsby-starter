c2f38a71f107b238c7d2ebe4fea95cd8fa4306cb
Rizchel Wood <rizchelwood@gmail.com>
2019-05-03 18:11:21 -0600

    make another gatsby folder

---
+++ test/src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Hi people! Testing, testing</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>It is time to build something amazing.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Page 2 this way</Link>
+  </Layout>
+)
+
+export default IndexPage
